61e2d185a50eb46f6552da53ae70ee2e01bd10aa
reber_logan <31495034+reberlvvc@users.noreply.github.com>
2020-04-20 16:42:38 -0500

    Done with Day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,28 @@
 
 import sys
 
+# unit 0 - ALU , 1 - PC Mutator, 2 - Other
+
+OPS = {
+    0b00000001: {"unit": 1, "ops": 0, "code": 'HLT'},
+    0b10000010: {"unit": 1, "ops": 2, "code": 'LDI'},
+    0b01000111: {"unit": 1, "ops": 1, "code": 'PRN'},
+}
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.reg = [0]*8
+        self.ram = [0]*256
+        self.pc = 0
+
+    def ram_read(self, loc):
+        return self.ram[loc]
+
+    def ram_write(self, loc, val):
+        self.ram[loc] = val
 
     def load(self):
         """Load a program into memory."""
@@ -18,27 +34,36 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
+        # LDI
         else:
             raise Exception("Unsupported ALU operation")
+
+    def cu(self, op, reg_a, reg_b):
+        if op == "HLT":
+            return
+        elif op == 'LDI':
+            self.reg[reg_a] = reg_b
+        elif op == 'PRN':
+            print(self.reg[reg_a])
+        else:
+            raise Exception("Unsupported CU operation")
 
     def trace(self):
         """
@@ -46,10 +71,10 @@ class CPU:
         from run() if you need help debugging.
         """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
+        print(f"TRACE: %02X %02X | %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -61,5 +86,22 @@ class CPU:
         print()
 
     def run(self):
-        """Run the CPU."""
-        pass
+        self.pc = 0
+        ir = self.ram_read(self.pc)
+        instruction = OPS[ir]["code"]
+        while instruction != 'HLT':
+            if OPS[ir]['unit'] == 0:
+                self.alu(
+                    instruction,
+                    self.ram_read(self.pc+1),
+                    self.ram_read(self.pc+2)
+                )
+            else:
+                self.cu(
+                    instruction,
+                    self.ram_read(self.pc+1),
+                    self.ram_read(self.pc+2)
+                )
+            self.pc += 1 + OPS[ir]['ops']
+            ir = self.ram_read(self.pc)
+            instruction = OPS[ir]["code"]
